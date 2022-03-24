ca219b9cdef0c6b1340730a8f74d4ba11db4cf85
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-03-24 18:58:46 +0100

    done

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -4,29 +4,29 @@ User class
 """
 
 class User():
+    """ Documentation """
+
+    __email = None
+
+    def __init__(self):
         """ Documentation """
+        self.__email = None
 
-            __email = None
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
 
-                def __init__(self):
-                            """ Documentation """
-                                    self.__email = None
+    @email.setter
+    def email(self, value):
+        """ Documentation """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
+   
+    
+if __name__ == "__main__":
 
-                                        @property
-                                            def email(self):
-                                                        """ Documentation """
-                                                                return self.__email
-
-                                                                @email.setter
-                                                                    def email(self, value):
-                                                                                """ Documentation """
-                                                                                        if type(value) is not str:
-                                                                                                        raise TypeError("email must be a string")
-                                                                                                            self.__email = value
-
-
-                                                                                                            if __name__ == "__main__":
-
-                                                                                                                    u = User()
-                                                                                                                        u.email = "john@snow.com"
-                                                                                                                            print(u.email)
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
