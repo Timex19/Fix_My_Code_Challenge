9eb8647df04c32126436ef9ff1df0ce91bb522d1
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-03-24 18:55:47 +0100

    Permission update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,29 +3,29 @@
 
 
 class square():
-        """Square class."""
-            width = 0
-                height = 0
+    """Square class."""
+    width = 0
+    height = 0
 
-                    def __init__(self, *args, **kwargs):
-                                """Init sequence."""
-                                        for key, value in kwargs.items():
-                                                        setattr(self, key, value)
+    def __init__(self, *args, **kwargs):
+        """Init sequence."""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-                                                            def area(self):
-                                                                        """ Area of the square """
-                                                                                return self.width * self.height
+    def area(self):
+        """ Area of the square """
+        return self.width * self.height
 
-                                                                                def perimeter(self):
-                                                                                            """ Perimeter of the square """
-                                                                                                    return (self.width * 2) + (self.height * 2)
+    def perimeter(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
 
-                                                                                                    def __str__(self):
-                                                                                                                """String representation."""
-                                                                                                                        return "{}/{}".format(self.width, self.height)
+    def __str__(self):
+        """String representation."""
+        return "{}/{}".format(self.width, self.height)
 
-                                                                                                                    if __name__ == "__main__":
-                                                                                                                            s = square(width=12, height=9)
-                                                                                                                                print(s)
-                                                                                                                                    print(s.area())
-                                                                                                                                        print(s.perimeter())
+if __name__ == "__main__":
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area())
+    print(s.perimeter())
